12709aeafb539059f5cc1fd4fbb1e13780b5e8e2
Angerstoner <angerstoner@mailbox.org>
2018-12-23 21:16:58 +0100

    first usable version (TOTP only)

--- rofi-otp.py
+++ rofi-otp.py
@@ -1,5 +1,5 @@
 import dmenu
-from subprocess import call, Popen, PIPE
+from subprocess import call
 import re
 import pyotp
 
@@ -7,17 +7,20 @@ entries = []
 
 class Entry:
     def __init__(self, title, user, secret, type):
+        self.id = -1
         self.title = title
         self.user = user
         self.secret = secret
         self.type = type
 
     def __repr__(self):
-        return self.title + "\t" + self.user
+        return str(self.id) + ": " + self.title + "\t" + self.user
 
     def __str__(self):
-        return self.title + "\t" + self.user
+        return str(self.id) + ": " + self.title + "\t" + self.user
 
+    def setID(self, id):
+        self.id = id
 
     def getTitle(self):
         return self.title
@@ -31,10 +34,11 @@ class Entry:
     def getType(self):
         return self.type
 
-
+    def getID(self):
+        return self.id
 
 def getAvailableCodes():
-    return entries
+    return [str(entry) for entry in entries]
 
 def typeToken(entry):
     totp = pyotp.TOTP(entry.getSecret())
@@ -44,16 +48,21 @@ def parseEntry(line):
     reg = re.search("([^;\n]+);([^;\n]+);([^;\n]+);([^;\n]+)", line)
     return Entry(reg.group(1), reg.group(2), reg.group(3), reg.group(4))
 
+def parseSelection(line):
+    return entries[int(re.search("(\d?):", line).group(1)) - 1]
+
+
 def initEntries():
     with open("SECRETS") as secrets:
+        i = 1
         for line in secrets.readlines():
-            entries.append(parseEntry(line))
+            entry = parseEntry(line)
+            entry.setID(i)
+            entries.append(entry)
+            i = i + 1
 
 def start():
     initEntries()
-    print(entries)
 
 start()
-
-
-typeToken(dmenu.show(getAvailableCodes()))
+typeToken(parseSelection(dmenu.show(getAvailableCodes())))
