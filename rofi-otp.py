c4b6b04cb06dcf3f450ce99e1e78f66b04f27b78
Angerstoner <angerstoner@mailbox.org>
2019-03-24 16:14:03 +0100

    added requirements.txt made rofi search case insensitiv

--- rofi-otp.py
+++ rofi-otp.py
@@ -4,46 +4,48 @@ from pyotp import TOTP
 from rofi import Rofi
 from pykeyboard import PyKeyboard
 from entry import Entry
-from addEntry import entryFromQR, openFromPath, openAsBase64Image, writeToFile
+from add_entry import entryFromQR, openFromPath, openAsBase64Image, writeToFile
 
 menu = "rofi"
 entries = []
 
 
-def showInDMenu(entries, prompt="Entries", mode="selection"):
+def show_in_dmenu(entries, prompt="Entries"):
     return dmenu.show(entries, prompt=prompt)
 
 
-def showInRofi(entries, prompt="Entries", mode="selection"):
-    if (mode == "prompt"):
+def show_in_rofi(entries, prompt="Entries", mode="selection"):
+    if mode == "prompt":
         return Rofi().text_entry(prompt)
-    index, key = Rofi().select(prompt, entries)
+    index, key = Rofi().select(prompt, entries, rofi_args=['-i'])
     return entries[index]
 
+
 menus = {
-    "dmenu": showInDMenu,
-    "rofi": showInRofi
+    "dmenu": show_in_dmenu,
+    "rofi": show_in_rofi
 }
 
-def getAvailableCodes():
+
+def get_available_codes():
     return [str(entry) for entry in entries]
 
 
-def typeToken(entry):
+def type_token(entry):
     totp = TOTP(entry.getSecret())
     PyKeyboard().type_string(totp.now())
 
 
-def parseEntry(line):
+def parse_entry(line):
     reg = re.search("([^;\n]+);([^;\n]+);([^;\n]+);([^;\n]+);(\d);([^;\n]+)", line)
     return Entry(reg.group(1), reg.group(2), reg.group(3), reg.group(4), reg.group(5), reg.group(6))
 
 
-def parseSelection(line):
+def parse_selection(line):
     return entries[int(re.search("(\d+):", line).group(1)) - 1]
 
 
-def addEntry():
+def add_entry():
     add_by_qr_path = "Add by QR-Code (path/url)"
     add_by_qr_image = "Add by QR-Code (base64 image)"
     add_manually = "Add manually"
@@ -60,46 +62,45 @@ def addEntry():
     writeToFile(new_entry)
 
 
-def editEntry():
+def edit_entry():
     print("EDIT")
 
 
-def showEntry():
+def show_entry():
     print("SHOW")
 
 
 entryOptions = {
-    "Add": addEntry,
-    "Edit": editEntry,
-    "Show": showEntry
+    "Add": add_entry,
+    "Edit": edit_entry,
+    "Show": show_entry
 }
 
 
-def initEntries():
+def init_entries():
     with open("SECRETS") as secrets:
         i = 1
         for line in secrets.readlines():
-            entry = parseEntry(line)
+            entry = parse_entry(line)
             entry.setID(i)
             entries.append(entry)
             i = i + 1
 
 
-def openMenu():
-    menulines = ["Add", "Edit", "Show"]
-    for entry in getAvailableCodes():
-        menulines.append(entry)
-    return menus[menu](menulines)
+def open_menu():
+    menu_lines = ["Add", "Edit", "Show"]
+    for entry in get_available_codes():
+        menu_lines.append(entry)
+    return menus[menu](menu_lines)
 
 
 def start():
-    initEntries()
-    selection = openMenu()
+    init_entries()
+    selection = open_menu()
     if selection in entryOptions:
         entryOptions[selection]()
     else:
-        typeToken(parseSelection(selection))
+        type_token(parse_selection(selection))
 
 
 start()
-
